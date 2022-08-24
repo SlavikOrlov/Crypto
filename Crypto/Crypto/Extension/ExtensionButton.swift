7c6490328364d93e6d64739a279cc49693804acd
Slava <Slava7711240@gmail.com>
2022-08-24 23:43:11 +0300

    Added Extension Button and Image

--- Crypto/Crypto/Extension/ExtensionButton.swift
+++ Crypto/Crypto/Extension/ExtensionButton.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+   
+    func createCustomButton(titleName: String, selector: Selector) -> UIBarButtonItem {
+        
+        let button = UIButton(type: .system)
+        button.tintColor = CustomColor.textColorButton
+        button.setTitle(titleName, for: .normal)
+        button.contentVerticalAlignment = .fill
+        button.contentHorizontalAlignment = .fill
+        button.addTarget(self, action: selector, for: .touchUpInside)
+        let menuBarItem =  UIBarButtonItem(customView: button)
+        return menuBarItem
+    }
+}
---
+++ Crypto/Crypto/Extension/ExtensionImages.swift
@@ -0,0 +1,22 @@
+//
+//  ExtensionImages.swift
+//  Crypto
+//
+//  Created by Slava Orlov on 24.08.2022.
+//
+
+import Foundation
+import UIKit
+
+struct ExtensionImage {
+    
+    //MARK: - Constants
+    
+    static let logoImage = UIImage(named: "coinLogo")
+    static let detailViewImage = UIImage(named: "coinLogo")
+    static let mainTab = UIImage(named: "mainTab")
+    static let profileTab = UIImage(named: "profileTab")
+    static let searchBar = UIImage(named: "searchBar")
+    static let passwordIsHidden = UIImage(named: "passwordIsHidden")
+    static let passwordIsShown = UIImage(named: "passwordIsShown")
+}
